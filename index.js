bc5f5130da41d65a6a7385998c77cb1e7464d1df
Josh Sanders <josh5231@gmail.com>
2017-03-06 05:36:36 +0000

    add package.json and basic node setup in index.js

---
+++ index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+var bodyParser = require('body-parser');
+
+const web_routes = require("./server/web_routes.js");
+const game_routes = require("./server/game_routes.js");
+
+const port = process.env.PORT || 8080;
+
+app.use("/",web_routes);
+app.use("/api/*",game_routes);
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.listen(port,(err)=>{
+    if(err){ throw err; }
+    console.log("App running on port:"+port);
+});
