084475f1669d714f0e0f210c95f5fe0ad44dfd90
Alex McLeod <alexmcleod01@gmail.com>
2017-03-07 04:10:43 +0000

    Added makeScene function

--- server/image_gen/image_gen.js
+++ server/image_gen/image_gen.js
@@ -2,8 +2,15 @@ var images = require('images');
 
 var exports = module.exports = {};
 
-exports.combineImages = function (backgroundImage, imageTwo, coordArray, saveImageTo) {
+exports.combineImages = function (backgroundImage, imageOverlay, coordArray) {
     return images(backgroundImage)
-         .draw(images(imageTwo), coordArray[0], coordArray[1])
-         .encode(saveImageTo, {quality: 50});
+         .draw(images(imageOverlay), coordArray[0], coordArray[1])
+         .encode("jpg", {operation: 50});
+};
+
+exports.makeScene = function (background, imageOne, imageTwo, coordArray, saveImageTo) {
+    images(background)
+        .draw(images(imageOne), coordArray[0], coordArray[1])
+        .draw(images(imageTwo), coordArray[2], coordArray[3])
+        .save(saveImageTo);
 };
