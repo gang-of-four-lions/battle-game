4bb54a16c3e8722eb3dcb1f62f4305db80c5daab
Josh Sanders <josh5231@gmail.com>
2017-03-07 06:51:51 +0000

    Added models

---
+++ server/db_manager/models/objs.js
@@ -0,0 +1,34 @@
+"use strict";
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const objectSchema = new Schema({
+    
+    _id: { },
+    name: { type:String, required:true },
+    
+    GameData: {
+       type: { type:String, required:true }, //weapon / armor / potion / ect...
+       effects: {  //The effect is has on each element or stats(HP/MP)
+            water: { type:Number, default:0 },
+            fire: { type:Number, default:0 },
+            earth: { type:Number, default:0 },
+            wood: { type:Number, default:0 },
+            metal: { type:Number, default:0 },
+            HP:{ type:Number, default:0 },
+            MP:{ type:Number, default:0 }
+            
+       },
+       action: String, //The associated action's id from Action-DB if any
+       },
+       
+    ShopData: {
+       imgURL: { type:String, required:true },
+       cost: { type:Number, min:0, default:0 },
+       requirements: {} //What is required to buy this item. 
+       }
+});
+
+var Obj = mongoose.model('Obj', objectSchema);
+
+export default Obj;
---
+++ server/db_manager/models/users.js
@@ -0,0 +1,62 @@
+"use strict";
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    
+    _id: { },
+    name: { type:String, required:true },
+    slackID: { type:String, required:true },
+    
+    playerStats: {
+        HP:  { type:Number, min:1, default:10 },
+        MP:  { type:Number, min:0, default:0 },
+        EXP:  { type:Number, min:0, default:0 },
+        LVL:  { type:Number, min:0, default:0 },
+        Class:String,
+        
+        resistants: { 
+            water: { type:Number, min:0, default:0 },
+            fire: { type:Number, min:0, default:0 },
+            earth: { type:Number, min:0, default:0 },
+            wood: { type:Number, min:0, default:0 },
+            metal: { type:Number, min:0, default:0 }
+        },
+        
+        attackBounces: { 
+            water: { type:Number, min:0, default:0 },
+            fire: { type:Number, min:0, default:0 },
+            earth:{ type:Number, min:0, default:0 },
+            wood: { type:Number, min:0, default:0 },
+            metal:{ type:Number, min:0, default:0 }
+        },
+        
+        actions: [], //Base actions assigned with charater class
+        
+        displayData: {
+            frontImage: String,
+            backImage: String,
+            wins:  { type:Number, min:0, default:0 },
+            gamesPlayed:  { type:Number, min:0, default:0 }
+        },
+        
+        inventory: {
+            objs: [], //An array of IDs for all objects this player has
+            coin: Number // $
+        },
+        
+        PlayerBuild:{ 
+            classtype: String,
+	        head: String,
+	        body: String,
+	        hair: String,
+	        shirt: String,
+	        pants: String
+        }
+    }
+    
+});
+
+var User = mongoose.model('User', userSchema);
+
+export default User;
