caaf6aa2a469e79c6ff341b1553b09b1824c018e
Josh Sanders <josh5231@gmail.com>
2017-03-08 05:06:59 +0000

    reworked tests and I'm working on interface

--- server/db_manager/manager.js
+++ server/db_manager/manager.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGOURI || "mongodb://admin:slackdb$45@ds123050.mlab.com:23050/battle_bot");
+mongoose.connect(process.env.MONGOURI);
 
 var exports = module.exports = {};
 const User = require('./models/users.js');
