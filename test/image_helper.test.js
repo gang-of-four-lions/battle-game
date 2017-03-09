103a2c813d9f83e6fd0b4f2be8372dd74fb234bc
Alex McLeod <alexmcleod01@gmail.com>
2017-03-09 05:11:02 +0000

    @josh fixed test

--- test/image_helper.test.js
+++ test/image_helper.test.js
@@ -3,9 +3,10 @@ var assert = require('chai').assert;
 
 //Test_background.jpg has a width of 500pixels and a height of 494pixels
 //Percent should convert to coords starting with top left corner
-/*describe('convertPercentToCoords', function() {
+describe('convertPercentToCoords', function(done) {
   it('Should convert stated as a fraction to pixel values', function(done) {
     var converted = convertPercentToCoords(__dirname + '/background.jpg', .25, .25);
     assert.deepEqual(converted, [125, 123.5]);
+    done();
   });
-});*/
+});
