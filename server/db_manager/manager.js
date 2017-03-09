3a715cc26bd9a5598634e308d2998e9c929629e6
Josh Sanders <josh5231@gmail.com>
2017-03-09 09:06:00 +0000

    more work done on manager and tests

--- server/db_manager/manager.js
+++ server/db_manager/manager.js
@@ -1,28 +1,69 @@
 "use strict";
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.MONGOURI);
+mongoose.connect(process.env.MONGOURI );
 
 var exports = module.exports = {};
 const User = require('./models/users.js');
+const Action = require('./models/actions.js');
 
-/*
-exports.User = require('./models/users.js');
-exports.Action = require('./models/actions.js');
-exports.Obj = require('./models/objs.js');
-*/
+//Helper Functions
 
-function lookUpUser(name,cb){
-    if(typeof(name)!=="string" || name===""){ cb("Invaild name input, name must be a valid string.",null); return; }
-    User.find({ name:name },(err,doc)=>{
-        if(err || doc.length===0){ cb("Error finding "+name,null); return; }
+const lookUpTable = {
+    "id": [ "id" ],
+    "name": [ "name" ],
+    "slackID": [ "slackID" ],
+    "HP": [ "playerStats","HP "],
+    "MP": [ "playerStats","MP" ],
+    "EXP": [ "playerStats","EXP" ],
+    "LVL": [ "playerStats","LVL" ],
+    "CLASS": [ "playerStats","CLASS" ],
+    "resWater": [ "playerStats","resistants","water" ],
+    "resFire": [ "playerStats","resistants","fire" ],
+    "resEarth": [ "playerStats","resistants","earth" ],
+    "resWood": [ "playerStats","resistants","wood" ],
+    "resMetal": [ "playerStats","resistants","metal" ],
+    
+    "atcWater": [ "playerStats","attackBounces","water" ],
+    "atcFire": [ "playerStats","attackBounces","fire" ],
+    "atcEarth": [ "playerStats","attackBounces","earth" ],
+    "atcWood": [ "playerStats","attackBounces","wood" ],
+    "atcMetal": [ "playerStats","attackBounces","metal" ],
+    
+    "coin": [ "playerStats","inventory","coin" ],
+    "frontImage": [ "displayData","frontImage" ],
+    "backImage":[ "displayData","backImage" ],
+    "wins":[ "displayData","wins" ],
+    "gamesPlayed": [ "displayData","gamesPlayed" ],
+    "classType": [ "PlayerBuild","classtype" ],
+    "head": [ "PlayerBuild","head" ],
+    "body": [ "PlayerBuild","body" ],
+    "shirt": [ "PlayerBuild","shirt" ],
+    "pants": [ "PlayerBuild","pants" ]
+};
+
+function lookUpUser(slackID,cb){
+    if(typeof(slackID)!=="string" || slackID===""){ cb("Invaild name input, name must be a valid string.",null); return; }
+    User.find({ slackID:slackID },(err,doc)=>{
+        if(err || doc.length===0){ cb("Error finding "+slackID,null); return; }
        cb(null,doc[0]);
        return;
     });
 };
 
-exports.getUser=function(name,cb){
-    lookUpUser(name,cb);
+function lookUpAction(name,cb){
+    if(typeof(name)!=="string" || name===""){ cb("Invaild name input, name must be a valid string.",null); return; }
+    Action.find({ name:name },(err,doc)=>{
+        if(err || doc.length===0){ cb("Error finding "+name+" - "+err,null); return; }
+       cb(null,doc[0]);
+       return;
+    });
+};
+
+//User Functions
+
+exports.getUser=function(slackID,cb){
+    lookUpUser(slackID,cb);
 };
 
 exports.createUser=function(data,cb){
@@ -35,35 +76,68 @@ exports.createUser=function(data,cb){
     });
 };
 
-exports.removeUser=function(name,cb){
-     if(typeof(name)!=="string" || name===""){ cb("Invaild name input, name must be a valid string.",null); return; }
-     User.remove({ name:name },(err)=>{
-        if(err){ cb("Error removing "+name,null); return; }
+exports.removeUser=function(slackID,cb){
+     if(typeof(slackID)!=="string" || slackID===""){ cb("Invaild name input, name must be a valid string.",null); return; }
+     User.remove({ slackID:slackID },(err)=>{
+        if(err){ cb("Error removing "+slackID,null); return; }
         cb(null,"done");
         return;
      });
 };
 
-exports.getInventory=function(userName,cb){
-    lookUpUser(userName,(err,doc)=>{
+exports.getInventory=function(slackID,cb){
+    lookUpUser(slackID,(err,doc)=>{
        if(err){ cb(err,null); return; }
        cb(null,doc.playerStats.inventory);
        return;
     });
 };
 
-exports.getDisplay=function(userName,cb){
-    lookUpUser(userName,(err,doc)=>{
+exports.getDisplay=function(slackID,cb){
+    lookUpUser(slackID,(err,doc)=>{
        if(err){ cb(err,null); return; }
        cb(null,doc.displayData);
        return;
     });  
 };
 
-exports.getStats = function(userName,cb){
-    lookUpUser(userName,(err,doc)=>{
+exports.getStats = function(slackID,cb){
+    lookUpUser(slackID,(err,doc)=>{
        if(err){ cb(err,null); return; }
        cb(null,doc.playerStats);
        return;
     });   
+};
+
+
+exports.updateUser = function(_user,cb){
+   if(!_user || !_user.name || !_user.slackID){ cb("Invaild user.",null); return; }
+        //User.remove({ slackID:_user.slackID },(err)=>{
+        //if(err){ cb("Error updating "+_user.slackID,null); return; }
+        _user.save( (err)=>{
+            if(err){ cb("Error saving user",null); return; }
+            console.log("User saved!");
+            cb(null,"Done");
+            return;
+            });
+        //});
+   }; 
+   
+exports.updateUser = function(slackID,obj,cb){
+    if(obj===null || typeof(obj)!="object" ){ cb("Invaild object",null); return; }
+    lookUpUser(slackID,(err,doc)=>{
+        if(err){ cb(err,null); return; }
+        for(let prop in obj) { 
+         if(lookUpTable[prop]!==undefined){
+             if(lookUpTable[prop].length===1){ doc[ lookUpTable[prop][0] ]= obj[prop]; }
+             else if(lookUpTable[prop].length===2){ doc[ lookUpTable[prop][0] ][ lookUpTable[prop][1] ]= obj[prop]; }
+             else if(lookUpTable[prop].length===3){ doc[ lookUpTable[prop][0] ][ lookUpTable[prop][1] ][ lookUpTable[prop][2] ]= obj[prop]; }
+             else if(lookUpTable[prop].length===4){ doc[ lookUpTable[prop][0] ][ lookUpTable[prop][1] ][ lookUpTable[prop][2] ][ lookUpTable[prop][3] ]= obj[prop]; }
+         }
+        }
+        doc.save(err=>{
+           if(err){ cb("Error saving doc",null); return; }
+           cb(null,"Updated");
+        });
+    });
 };
--- test/db.test.js
+++ test/db.test.js
@@ -6,7 +6,7 @@ const manager = require("../server/db_manager/manager.js");
 
 describe('DB-Manager.createUser()', function() {
       it('should add a entry to the DB', (done) => {
-          manager.createUser({ name:"Tester", slackID:"tester"}, (err,doc)=>{
+          manager.createUser({ name:"TesterName", slackID:"Tester"}, (err,doc)=>{
              if(err){ done(err); }
              else { done(); }
           });
@@ -42,6 +42,36 @@ describe('DB-Manager.getUser()', function() {
           });
       });
 });
+
+
+
+describe('DB-Manager.updateUser()', function() {
+    
+      it('should find and update entry on DB', (done) => {
+        manager.updateUser("Tester",
+        {
+            "LVL":10,
+            "resWater":20,
+            "atcEarth":25,
+            "coin":100
+        }
+        ,(err)=>{
+           if(err){ done(err); return; } 
+           else { done(); }
+        }); 
+      });
+      
+      it('it should have saved the data to the DB', (done) => {
+         manager.getUser("Tester",(err,doc)=>{
+            if(err){ done(err,null); return; }
+            if(doc.playerStats.LVL!==10 || doc.playerStats.resistants.water!==20){ done("Did not Save",null); return; }
+            else { done(); }
+         });
+      });
+
+});
+
+
 
 describe('DB-Manager.removeUser()', function() {
       it('should remove user from DB', (done) => {
