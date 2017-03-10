79b685698ca74453d986798a70259e92717d8ff9
Vladimir Logachev <doit@keemail.me>
2017-03-10 20:12:20 +0700

    added some comments

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -3,24 +3,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    main: './client/src/index.js'
+    main: './client/src/index.js' // MAIN chunk will contain only application code (MANIFEST and VENDOR will be extracted from MAIN)
   },
   output: {
     path: './client/build',
-    filename: '[name].[chunkhash].js',
+    filename: '[name].[chunkhash].js', // this is necessary to cache non-changed vendor files and update frequently changed App code
   },
   plugins: [
-    new HtmlWebpackPlugin({
+    new HtmlWebpackPlugin({ // this is necessary for making HTML on-the fly (because we don't have constant assets names now)
       title: 'Battle Game LP',
       filename: 'index.html',
     }),
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor', // vendor chunk name
+    new webpack.optimize.CommonsChunkPlugin({ // VENDOR chunk will contain only node_modules
+      name: 'vendor',
       minChunks: function(module) {
-        return module.context && module.context.indexOf('node_modules') !== -1; // this assumes your vendor imports exist in the node_modules directory
+        return module.context && module.context.indexOf('node_modules') !== -1;
       }
     }),
-    new webpack.optimize.CommonsChunkPlugin({
+    new webpack.optimize.CommonsChunkPlugin({ // MANIFEST chunk will contain only webpack runtime code
       name: "manifest",
       minChunks: Infinity
     })
@@ -28,7 +28,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js$/, // All JavaScript code - into Babel
         use: [
           'babel-loader',
         ],
