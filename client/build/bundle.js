1dbf76bc0ad08d760cfa82e5f5eee7a370f146ec
Vladimir Logachev <doit@keemail.me>
2017-03-10 15:24:19 +0700

    fixed problem with static files

--- client/build/bundle.js
+++ client/build/bundle.js
@@ -8,3 +8,4 @@
 // );
 
 alert('test');
+console.log('test');
--- server/server.js
+++ server/server.js
@@ -3,17 +3,20 @@
 const express = require("express");
 const app = express();
 var bodyParser = require('body-parser');
+const path = require('path');
 
 const web_routes = require("./web_routes.js");
 const game_routes = require("./game_routes.js");
 
 app.set('port', process.env.PORT || 8080);
 
-app.use("/", web_routes);
-// app.use("/api/*", game_routes);
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-// app.use(bodyParser.urlencoded({
-//   extended: <true></true>
-// }));
+app.use(express.static(path.join(__dirname, '..', 'client')));
+
+// app.use("/api/*", game_routes);
+app.use("/", web_routes);
 
 module.exports = app;
